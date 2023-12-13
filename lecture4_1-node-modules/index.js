d18a1390de154c4cfcf7c6d8891f1af26a63c978
RajKoladiya22 <koladiyaraj22@gmail.com>
2023-12-13 12:52:45 +0530

    lecture-4

---
+++ lecture4_1-node-modules/index.js
@@ -0,0 +1,11 @@
+const raj = require('./module');
+
+
+let ans = raj.sum(10,2);
+let min = raj.min(10,2)
+
+console.log(`Sum = ${ans}`);
+console.log(`Min = ${min}`);
+
+
+
---
+++ lecture4_1-node-modules/module.js
@@ -0,0 +1,9 @@
+const sum = (a , b) => {
+    return a + b;
+}
+
+const min = (a,b) => {
+    return a-b;
+}
+
+module.exports = {sum,min};
